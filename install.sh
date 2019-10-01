3e27bb27db64323fb4b1c83d6f3a08afb99f775d
João Marcos <marcospb19@hotmail.com>
2019-10-01 13:25:25 -0300

    Added an installation script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+#
+# This is written in bash for the sake of simplicity
+#
+
+echo "This script is bad"
+
+if [[ "$#" != 0 ]]; then
+	echo "This script takes no arguments"
+	exit 1
+elif [[ ! -d "$HOME/.config/sublime-text-3" ]]; then
+	echo "Sublime configuration folder not found at $HOME/.config/sublime-text-3"
+	exit 1
+elif [[ ! -d "$HOME/.config/sublime-text-3/Packages" ]]; then
+	echo "Could not found Packages folder at $HOME/.config/sublime-text-3/Packages"
+	exit 1
+fi 
+
+set -e
+
+function safe-cd()
+{
+	cd "$1" || exit 1
+}
+
+themes_dir=$(pwd)
+
+safe-cd "$HOME/.config/sublime-text-3/Packages"
+
+mkdir '\-One-Dark-Package-'
+cp "${themes_dir}/One Dark.tmTheme" '\-One-Dark-Package-/'
+
+mkdir '\-Eighties-Dark-Package-'
+cp "${themes_dir}/Eighties Dark.tmTheme" '\-Eighties-Dark-Package-/'
+
+echo "Is everything working? Ok..."
+
+set +e
--- readme.md
+++ readme.md
@@ -1,7 +1,12 @@
 # sublime-text-themes
 My favourite Sublime themes
 
-Sorry for the bad preview pictures, i'm lazy
+### Installation
+`./install.sh`
+
+This will run a very simple script i created that creates 2 directories under the configuration folder and copies each theme inside
+
+Sorry for the bad preview pictures, i'm busy
 
 ### Eighties Dark
 ![Eighties preview](/previews/eighties-dark-preview.jpg)
@@ -9,3 +14,4 @@ Sorry for the bad preview pictures, i'm lazy
 ### One Dark
 ![One Dark preview](/previews/one-dark-preview.jpg)
 
+# :D
